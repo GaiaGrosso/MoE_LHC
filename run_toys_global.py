1e7e6da1ee66fb1ef013935e17c3b158c8222e3f
GaiaGrosso <gaia.grosso@cern.ch>
2025-07-08 10:35:46 -0400

    global gate

--- run_toys_global.py
+++ run_toys_global.py
@@ -2,7 +2,6 @@ import os, json, argparse, glob, time, datetime
 import numpy as np
 import os.path
 
-# specify here where you want to save the output of your training
 OUTPUT_DIRECTORY =  './out/'
 
 def create_config_file(config_table, OUTPUT_DIRECTORY):
@@ -12,25 +11,36 @@ def create_config_file(config_table, OUTPUT_DIRECTORY):
 
 # configuration dictionary
 config_json = {
-    "N_Ref"   : 17700,#number of bkg events in class 0
-    "N_Bkg"   : 1770, #number of bkg events in class 1
-    "N_Sig"   : 36*2, #number of signal events in class 1
-    "signal" : 'Zp3500-Hc800', # it loops over sasmples ['Zp3500-Hc800_HWlWh1-3400', 'Zp3500-Hc800_HWlWh2-3400'],
-    "w_ref": 0.1, #luminosity ratio between class 1 and class 0 
+    "N_Ref"   : 17700,#number of bkg events in class 0                          
+    "N_Bkg"   : 1770, #number of bkg events in class 1                                                 
+    "N_Sig"   : 36*2, #number of signal events in class 1                 
+    "signal" : 'Zp3500-Hc800', # it loops over sasmples ['Zp3500-Hc800_HWlWh1-3400', 'Zp3500-Hc800_HWlWh2-3400']
+    "w_ref": 0.1, #luminosity ratio between class 1 and class 0                          
     "output_directory": OUTPUT_DIRECTORY,
     "epochs": 10,
-    "patience": 1, #interval between saved monitored metrics
-    "plt_patience": 10, #interval between new plots generation (if plot==True)
-    "plot": True, #whether to make plots
+    "patience": 1, #interval between saved monitored metrics                                                                        
+    "plt_patience": 10, #interval between new plots generation (if plot==True)                                                  
+    "plot": True, #whether to make plots                                                                   
     "learning_rate" : 0.001,
-    "n_kfold": 2, # number of folds in the k-fold procedure
+    "n_kfold": 2, # number of folds in the k-fold procedure                                                    
+    "gate_coeffs": [1. for _ in range(5)], # this is used as a mask for which experts to train on                                       
+    #"gate_coeffs": [1, 0, 0, 0, 0],                                                                                                     
+    "gate_train_coeffs" : True,                                                                                                         
     "L2_reg": 0.1,
     "Pois_ON": True, # whether data should have poissonian number of events
 }
 
+
 # training specs
-ID = ''
-ID += "L2reg%s"%(str(config_json["L2_reg"]))
+gate_str = ''
+if config_json["gate_train_coeffs"]:
+    gate_str += 'trainGate'
+else:
+    gate_str += 'frozenGate'
+for m in config_json["gate_coeffs"]:
+    gate_str += '-%s'%(str(m))
+gate_str += "L2reg%s"%(str(config_json["L2_reg"]))
+ID = gate_str
 ID += '_epochs%s'%(config_json["epochs"])
 ID += '_kfold%i'%(config_json["n_kfold"])
 ID += '_lr%s'%(str(config_json["learning_rate"]))
@@ -39,16 +49,15 @@ if "gather_after" in list(config_json.keys()):
 if not config_json["Pois_ON"]:
     ID += '_NO-Pois'
 
-# problem specs                                                                                                                
+# problem specs                                                                                                             
 ID +='/Nref'+str(config_json["N_Ref"])
 ID +='_Nbkg'+str(config_json["N_Bkg"])
 ID +='_Nsig'+str(config_json["N_Sig"])
 if config_json["N_Sig"]:
     ID += config_json["signal"]
-
 config_json["output_directory"] = OUTPUT_DIRECTORY+'/'+ID
 
-#### launch python script ###########################                 
+#### launch python script ###########################                                  
 if __name__ == '__main__':
     parser   = argparse.ArgumentParser()
     parser.add_argument('-p','--pyscript', type=str, help="name of python script to execute", required=True)
@@ -60,6 +69,7 @@ if __name__ == '__main__':
     pyscript = args.pyscript
     firstseed= args.firstseed
     config_json['pyscript'] = pyscript
+
     pyscript_str = pyscript.replace('.py', '')
     pyscript_str = pyscript_str.replace('_', '/')
     config_json["output_directory"] = OUTPUT_DIRECTORY+'/'+pyscript_str+'/'+ID
@@ -67,7 +77,7 @@ if __name__ == '__main__':
         os.makedirs(config_json["output_directory"])
 
     json_path = create_config_file(config_json, config_json["output_directory"])
-    
+
     if args.local:
         # launch the script locally
         if firstseed<0:
@@ -76,7 +86,7 @@ if __name__ == '__main__':
         else:
             os.system("python %s/%s -j %s -s %i"%(os.getcwd(), pyscript, json_path, firstseed))
     else:
-        # submit the script via slurm job
+        # submit the script via slurm job                                    
         # (this is useful if you have access to computing resources in a cluster)
         label = "logs"
         os.system("mkdir %s" %label)
@@ -90,10 +100,10 @@ if __name__ == '__main__':
             script_sbatch.write("#!/bin/bash\n")
             script_sbatch.write("#SBATCH -c 1\n")
             script_sbatch.write("#SBATCH --gpus 1\n")
-            script_sbatch.write("#SBATCH -t 0-3:00\n")
+            script_sbatch.write("#SBATCH -t 0-2:00\n")
             script_sbatch.write("#SBATCH -p iaifi_gpu_priority\n")
             #script_sbatch.write("#SBATCH -p gpu\n")
-            #script_sbatch.write("#SBATCH -p serial_re\n")                                                                           
+            #script_sbatch.write("#SBATCH -p serial_re\n")                                                 
             script_sbatch.write("#SBATCH --mem=5000\n")
             script_sbatch.write("#SBATCH -o ./logs/%s"%(pyscript_str)+"_%j.out\n")
             script_sbatch.write("#SBATCH -e ./logs/%s"%(pyscript_str)+"_%j.err\n")
--- toy_global_kfold_sigmoid.py
+++ toy_global_kfold_sigmoid.py
@@ -1,15 +1,15 @@
 import glob, h5py, math, time, os, json, random, yaml, argparse, datetime
 from torch.utils.data import DataLoader, Dataset
 from scipy.stats import norm, expon, chi2, uniform, chisquare
-from pathlib import Path
 from sklearn.model_selection import KFold
 from torch.utils.data import Subset
+from pathlib import Path
 from numpy.random import choice
 import pandas as pd
 import numpy as np
-import gc
+import gc 
 import matplotlib as mpl
-#mpl.use('Agg')
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as font_manager
 plt.rcParams["font.family"] = "serif"
@@ -19,43 +19,42 @@ from NNutils import *
 from PLOTutils import *
 from GENutils import *
 parser   = argparse.ArgumentParser()
+
 parser.add_argument('-j', '--jsonfile', type=str, help="json file", required=False, default=None)
 parser.add_argument('-s', '--seed', type=int, help="toy seed", required=False, default=None)
 args     = parser.parse_args()
 
-# random seed                                                                                                        
+# random seed
 seed = args.seed
 if seed==None:
     seed = datetime.datetime.now().microsecond+datetime.datetime.now().second+datetime.datetime.now().minute
 np.random.seed(seed)
 print('Random seed:'+str(seed))
 
-# train on GPU?                                                                                                      
-cuda = torch.cuda.is_available()
+# train on GPU?
+cuda = True
 DEVICE = torch.device("cuda" if cuda else "cpu")
 
 # setup parameters
-json_path = args.jsonfile
-if json_path !=None:
-    with open(json_path, 'r') as jsonfile:
+if args.jsonfile !=None:
+    with open(args.jsonfile, 'r') as jsonfile:
         config_json = json.load(jsonfile)
 
-plot =  config_json["plot"]
-# problem definition                                                                                                 
+plot =	config_json["plot"]
+# problem definition
 N_ref      = config_json["N_Ref"]
 N_Bkg      = config_json["N_Bkg"]
 N_Sig      = config_json["N_Sig"]
 z_ratio    = config_json["w_ref"]
 Pois_ON    = config_json["Pois_ON"]
-print(N_Sig)
+
 N_bkg_p, N_sig_p = N_Bkg,N_Sig
 if Pois_ON:
     N_bkg_p = np.random.poisson(N_Bkg, size=(1,))[0]
     N_sig_p = np.random.poisson(N_Sig, size=(1,))[0]
 
 signal_key = config_json["signal"]
-
-##### define output path ######################                                                                      
+##### define output path ######################  
 OUTPUT_PATH    = config_json["output_directory"]
 OUTPUT_FILE_ID = '/seed%s/'%(seed)
 folder_out = OUTPUT_PATH+OUTPUT_FILE_ID
@@ -63,14 +62,13 @@ if not os.path.exists(folder_out):
     os.makedirs(folder_out)
 output_folder = folder_out
 
-# DATA                                                                                                               
-## ref and bkg                                                                                                       
+# DATA
+## ref and bkg
 data = []
 for key in ['Wjj-3400', 'WWj-3400']:
     file = files[key]
     data.append(pd.read_csv(file,delim_whitespace = True,index_col =None).to_numpy())
 data = np.concatenate(data, axis=0)
-print(data.shape[0])
 # draw from BKG data according to the weights
 probability_distribution = data[:, -1]/np.sum(data[:, -1])
 list_of_candidates_idx = np.arange(len(data))
@@ -78,57 +76,60 @@ number_of_items_to_pick = N_ref+N_bkg_p
 draw = choice(list_of_candidates_idx, number_of_items_to_pick, replace=True,
               p=probability_distribution)
 data = data[draw]
-data = data[:, :-1] # remove weights from input data                                                                 
-## sig 
+data = data[:, :-1] # remove weights from input data
+
+## sig
 data_s = []
 for key in ['%s_HWlWh1-3400'%(signal_key), '%s_HWlWh2-3400'%(signal_key)]:
     file_s = files[key]
     data_s.append(pd.read_csv(file,delim_whitespace = True,index_col =None).to_numpy())
-# draw from SIG data according to the weights
 data_s = np.concatenate(data_s, axis=0)
+# draw from SIG data according to the weights
 probability_distribution = data_s[:, -1]/np.sum(data_s[:, -1])
 list_of_candidates_idx = np.arange(len(data_s))
 number_of_items_to_pick = N_sig_p
 draw = choice(list_of_candidates_idx, number_of_items_to_pick, replace=True,
               p=probability_distribution)
 data_s = data_s[draw]
-data_s = data_s[:, :-1] # remove weights from input data                                                             
+data_s = data_s[:, :-1] # remove weights from input data   
 
 # concatenate datasets
 feature = np.concatenate([data_s, data], axis=0)
-target = np.concatenate([np.ones((N_sig_p+N_bkg_p, 1)), np.zeros((N_ref,1))], axis=0)
+target  = np.concatenate([np.ones((N_sig_p+N_bkg_p, 1)), np.zeros((N_ref,1))], axis=0)
 weights = np.concatenate([np.ones((N_sig_p+N_bkg_p, 1)), z_ratio*np.ones((N_ref,1))], axis=0)
 print(feature.shape, target.shape)
 print(np.mean(feature, axis=0), np.std(feature, axis=0))
 
-# standardize                                                                                                        
+# standardize
 feature = standardize(feature, mean_bkg, std_bkg)
 
-# convert to torch tensors                                                                                           
+# convert to torch tensors
 feature = torch.from_numpy(feature).to(torch.float32)
 target  = torch.from_numpy(target).to(torch.float32)
 weights = torch.from_numpy(weights).to(torch.float32)
 
-# model definition                                                                                                   
-## gate                                                                                                              
-gate_architecture = [16, 16, 5]
-## experts                                                                                                           
-# specify which features are input to each expert
-input_idx_matrix = [
-    [0,1], # mWJJ, mJJ
-    [2,3,4,5], # pT ordered
-    [6,7,8,9], # mass ordered
-    [10,11,12], # score 1 
-    [13,14,15] # score 2
-]
+# model definition
+## gate
+gate_coeffs = torch.tensor(config_json["gate_coeffs"]).to(torch.float32).to(DEVICE)
+gate_train_coeffs = config_json["gate_train_coeffs"]
+print('gate_coeffs:', gate_coeffs)
 
-# specify architecture of each expert
+## experts
+# specify which features are input to each expert                                          
+input_idx_matrix = [
+    [0,1],      # mWJJ, mJJ                                                                                 
+    [2,3,4,5],  # pT ordered                                                                                                               
+    [6,7,8,9],  # mass ordered                                                                                                      
+    [10,11,12], # score 1                                                                                         
+    [13,14,15]  # score 2                                                                              
+]
+# specify architecture of each expert                                                                                                         
 exp_architecture = [
-    [2,3,3,1], # mWJJ, mJJ                                                                                           
+    [2,3,3,1], # mWJJ, mJJ                                                                                                  
     [4,6,6,1], # pT ordered                                                                                          
-    [4,6,6,1], # mass ordered                                                                                        
-    [3,5,5,1], # score 1                                                                                             
-    [3,5,5,1]  # score 2                                                                                             
+    [4,6,6,1], # mass ordered                                                                                              
+    [3,5,5,1], # score 1                                                                                                             
+    [3,5,5,1]  # score 2                                                                                                                 
 ]
 lr = config_json["learning_rate"]
 L2_regularizer = config_json["L2_reg"]
@@ -137,32 +138,35 @@ plt_patience = config_json["plt_patience"]
 total_epochs = np.array(config_json["epochs"])
 batch_size = int(feature.shape[0]/10)
 accumulation_steps = int(target.shape[0]/batch_size)
-n_experts = gate_architecture[-1]
-    
+n_experts = len(gate_coeffs)
+if not gate_train_coeffs:
+    n_experts = sum(x != 0 for x in gate_coeffs)
 train_data = CustomDataset(feature, target, weights)
+dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=False, drop_last=True, num_workers=1)
 
-# define the model
-model = MixtureOfExperts_LocalGate(
+model = MixtureOfExperts_GlobalGate(
     input_idx_matrix=input_idx_matrix,
     exp_architecture = exp_architecture,
-    gate_architecture=gate_architecture,
-    activation='sigmoid',
+    gate_coeffs=gate_coeffs,
+    gate_train_coeffs=gate_train_coeffs,
+    activation='sigmoid'
 ).to(DEVICE)
 
-# initialize log arrays
+# initialize log arrays                                                                                 
 # (they are filled at a rate defined by the patience)
 loss_history      = np.array([])
 val_loss_history  = np.array([])
 epochs_history    = np.array([])
 gate_history      = np.array([])
 
-# Define loss function and optimizer                                                                                 
+# Define loss function and optimizer
 criterion = NPLMLoss
 parameters = model.parameters()
 print("Trainable parameters:")
 print(count_parameters(model))
 optimizer_adam = torch.optim.Adam(parameters, weight_decay=L2_regularizer)
 
+# Training loop
 # Initialize KFold
 n_kfold_splits = config_json['n_kfold']
 kfold = KFold(n_splits=n_kfold_splits, shuffle=True, random_state=42)
@@ -170,12 +174,12 @@ kfold = KFold(n_splits=n_kfold_splits, shuffle=True, random_state=42)
 # Training loop                                                                                                      
 t1 = time.time()
 for epoch in range(total_epochs):
-    # Split the dataset into k folds
+    # Split the dataset into 5 folds
     for fold, (train_idx, val_idx) in enumerate(kfold.split(train_data)):
         # Create subsets for training and validation
         train_subset = Subset(train_data, train_idx)
         val_subset = Subset(train_data, val_idx)
-        if not (epoch%patience): print(f"Fold {fold}:")        
+        if not (epoch%patience):print(f"Fold {fold}:")
         train_loader = DataLoader(train_subset, batch_size=64, shuffle=True, num_workers=1)
         val_loader   = DataLoader(val_subset, batch_size=64, shuffle=False, num_workers=1)
 
@@ -191,16 +195,15 @@ for epoch in range(total_epochs):
                 loss_history = np.append(loss_history, train_loss)
                 val_loss_history = np.append(val_loss_history, val_loss)
                 epochs_history = np.append(epochs_history, epoch)
-                gate_tmp = gate_evaluation(model, criterion, val_loader, DEVICE,).cpu().detach().numpy().reshape((1, -1))
+                gate_tmp = model.get_gate_coeffs().cpu().detach().numpy().reshape((1,-1))
                 if not len(gate_history): gate_history = gate_tmp
                 else: gate_history = np.concatenate((gate_history, gate_tmp), axis=0)
             print('epoch: %i, loss: %f, val loss: %f'%(int(epoch+1),train_loss, val_loss))
             print('GATE COEFFS:', gate_history[-1, :])
-
-        if torch.isnan(train_loss):
+        
+        if np.isnan(train_loss):
             del train_loss, optimizer
             break
-        # check if skipping plots
         if plot==False: continue
         if ((epoch%plt_patience) or (epoch==0)) and (epoch!=(total_epochs-1)): continue
         # plots
@@ -221,14 +224,13 @@ for epoch in range(total_epochs):
         plt.show()
         plt.close()
         del fig, ax, font
-        
-        # evolution of the gate activation over time
-        # (activation is averaged over the dataset)
+
+        # gate evolution
         fig = plt.figure(figsize=(9,6))
         fig.patch.set_facecolor('white')
         ax= fig.add_axes([0.15, 0.1, 0.78, 0.8])
         for m in range(n_experts):
-            plt.plot(epochs_history[:], gate_history[:, m], label='Gate %i'%(m))
+            plt.plot(epochs_history, gate_history[:, m], label='Gate %i'%(m))
         font=font_manager.FontProperties(family='serif', size=14)
         plt.legend(prop=font)
         plt.ylabel('Coeffs', fontsize=18, fontname='serif')
@@ -241,7 +243,7 @@ for epoch in range(total_epochs):
         plt.close()
         del fig, ax, font
 
-        # histogram of classifier output for the training data
+        # hostogram of the classifier output
         fig = plt.figure(figsize=(9,6))
         fig.patch.set_facecolor('white')
         ax= fig.add_axes([0.15, 0.1, 0.78, 0.8])
@@ -249,16 +251,14 @@ for epoch in range(total_epochs):
             pred = model(feature.to(DEVICE))
             pred_norm = torch.sigmoid(pred[:, 0]).cpu().detach().numpy()
             print(pred_norm.shape)
-        w = weights[:, 0]                                                                          
+        w = weights[:, 0]
         bins = np.linspace(0., 1, 20)
-        hD = plt.hist(pred_norm[target[:, 0]==1],
-                      weights=w[target[:, 0]==1], bins=bins,
-                      label='DATA', color='black',
-                      lw=1.5, histtype='step', zorder=2)
-        hR = plt.hist(pred_norm[target[:, 0]==0],
-                      weights=w[target[:, 0]==0],
-                      color='#a6cee3', ec='#1f78b4', bins=bins,
-                      lw=1, label='REFERENCE', zorder=1)
+        hD = plt.hist(pred_norm[target[:, 0]==1],weights=w[target[:, 0]==1],
+                      bins=bins,label='DATA', color='black', lw=1.5,
+                      histtype='step', zorder=2)
+        hR = plt.hist(pred_norm[target[:, 0]==0], weights=w[target[:, 0]==0],
+                      color='#a6cee3', ec='#1f78b4',
+                      bins=bins, lw=1, label='REFERENCE', zorder=1)
         plt.errorbar(0.5*(bins[1:]+bins[:-1]), hD[0],
                      yerr= np.sqrt(hD[0]), color='black',
                      ls='', marker='o', ms=5, zorder=3)
@@ -275,28 +275,29 @@ for epoch in range(total_epochs):
         plt.show()
         plt.close()
         del hD, hR, font, l, ax, fig
-
-        # density ratio reconstruction projected on the various marginals
-        w = w.detach().numpy()                                                                                               
-        select_ref = 1*(target[:, 0]==0)                                                                                     
-        select_dat = 1*(target[:, 0]==1)                                                                                     
-        ref_preds = pred.cpu().detach().numpy()[select_ref>0]                                                                
-        dat = feature.detach().numpy()                                                                                       
-        for k in range(16):                                                                                                  
-            dat_k = dat[:, k:k+1]                                                                                            
-                                                                                                                         
-            plot_reconstruction(data=dat_k[select_dat>0],                                                                    
-                        weight_data=w[select_dat>0],                                                                     
-                        ref=dat_k[select_ref>0],                                                                         
-                        weight_ref=w[select_ref>0],                                                                      
-                        ref_preds=[ref_preds],                                                                           
-                        ref_preds_labels=['NPLM'],                                                                       
-                        centroids=[],                                                                                    
-                        t_obs=None, df=None,                                                                             
-                        file_name='reco%i_epoch%i.pdf'%(k,epoch), save=True,                                             
-                        save_path=output_folder,                                                                         
-                        xlabels=[labels_dict[str(k)]], yrange={labels_dict[str(k)]: [-2, 2]},                            
-                        bins=np.linspace(np.min(dat_k), np.max(dat_k), 30))   
+        ####
+        
+        w = w.detach().numpy()  
+        select_ref = 1*(target[:, 0]==0)
+        select_dat = 1*(target[:, 0]==1)
+        ref_preds = pred.cpu().detach().numpy()[select_ref>0]
+        dat = feature.detach().numpy()
+        for k in range(16):
+            dat_k = dat[:, k:k+1]
+            plot_reconstruction(data=dat_k[select_dat>0],
+                            weight_data=w[select_dat>0],
+                            ref=dat_k[select_ref>0],
+                            weight_ref=w[select_ref>0],
+                            ref_preds=[ref_preds],
+                            ref_preds_labels=['NPLM'],
+                            centroids=[],
+                            t_obs=None, df=None,
+                            file_name='reco%i_epoch%i.pdf'%(k,epoch), save=True,
+                            save_path=output_folder,
+                            xlabels=[labels_dict[str(k)]], yrange={labels_dict[str(k)]: [-2, 2]},
+                            bins=np.linspace(np.min(dat_k), np.max(dat_k), 30))
+        del ref_preds, dat
+        
 t2=time.time()
 print('End training')
 print('execution time: ', t2-t1)
@@ -306,21 +307,21 @@ with torch.no_grad():
     nplm_loss_final = NPLMLoss(target.to(DEVICE), weights.to(DEVICE), pred).cpu().detach().numpy()
 print('Final loss: ', nplm_loss_final)
 
-# save test statistic                                                                                                
+# save test statistic                                                             
 t_file=open(output_folder+'t.txt', 'w')
 t_file.write("%f\n"%(-2*nplm_loss_final))
 t_file.close()
 
-# save exec time                                                                                                     
+# save exec time                                              
 t_file=open(output_folder+'time.txt', 'w')
 t_file.write("%f\n"%(t2-t1))
 t_file.close()
 
-# save monitoring metrics                                                                                            
+# save monitoring metrics                                     
 np.save(output_folder+'loss_history', loss_history)
 np.save(output_folder+'gate_history', gate_history)
 
- # save model                                                                        
+# save model 
 checkpoint = {
     'weights': model.state_dict(),
     'optimizer': optimizer_adam.state_dict(),
